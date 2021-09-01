7b6b4263e829350d997b95af00ac2dd0d720a8b2
wangruohui <12756472+wangruohui@users.noreply.github.com>
2021-09-01 21:09:14 +0800

    update srun

--- srun.sh
+++ srun.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x 
 
 DATE=`date "+%m%d-%H%M"`
-EXP_NAME=$1
-GPUS=${2:-8}
-JOBNAME=ugatit-orig-${DATE}-${EXP_NAME}
+#EXP_NAME=$1
+GPUS=${1:-8}
+JOBNAME=ugatit-orig-${DATE}
 
 srun -p mediaf \
     --job-name=$JOBNAME \
@@ -11,5 +11,5 @@ srun -p mediaf \
     --ntasks=$GPUS \
     --cpus-per-task=4 \
     --kill-on-bad-exit=1 \
-    python main.py --light 8 --dataset selfie2anime \
+    python main.py --light 8 --dataset selfie2anime --save_freq 10000\
     2>&1 | tee ~/$JOBNAME.log
