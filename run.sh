86cd7efe4212f7b1736c18455908441a3db222e3
wangruohui <12756472+wangruohui@users.noreply.github.com>
2021-09-01 22:40:27 +0800

    add multi gpu script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x 
+
+DATE=`date "+%m%d-%H%M"`
+RESULT=result-${DATE}
+
+# python main.py --light 8 --dataset selfie2anime --save_freq 8000 --print_freq 10
+python main.py \
+    --light 8 \
+    --dataset selfie2anime \
+    --save_freq 8000 \
+    --result_dir $RESULT
+    2>&1 | tee ~/${RESULT}.log
