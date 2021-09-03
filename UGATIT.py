b198f3b60ff2579492ca6b826444bd7207b62840
wangruohui <12756472+wangruohui@users.noreply.github.com>
2021-09-03 19:40:52 +0800

    modify id loss

--- UGATIT.py
+++ UGATIT.py
@@ -232,8 +232,9 @@ class UGATIT(object) :
             fake_A2B2A, _, _ = self.genB2A(fake_A2B)
             fake_B2A2B, _, _ = self.genA2B(fake_B2A)
 
-            fake_A2A, fake_A2A_cam_logit, _ = self.genB2A(real_A)
-            fake_B2B, fake_B2B_cam_logit, _ = self.genA2B(real_B)
+            if self.identity_weight > 0:
+                fake_A2A, fake_A2A_cam_logit, _ = self.genB2A(real_A)
+                fake_B2B, fake_B2B_cam_logit, _ = self.genA2B(real_B)
 
             fake_GA_logit, fake_GA_cam_logit, _ = self.disGA(fake_B2A)
             fake_LA_logit, fake_LA_cam_logit, _ = self.disLA(fake_B2A)
@@ -255,11 +256,19 @@ class UGATIT(object) :
             G_identity_loss_A = self.L1_loss(fake_A2A, real_A)
             G_identity_loss_B = self.L1_loss(fake_B2B, real_B)
 
-            G_cam_loss_A = self.BCE_loss(fake_B2A_cam_logit, torch.ones_like(fake_B2A_cam_logit).to(self.device)) + self.BCE_loss(fake_A2A_cam_logit, torch.zeros_like(fake_A2A_cam_logit).to(self.device))
-            G_cam_loss_B = self.BCE_loss(fake_A2B_cam_logit, torch.ones_like(fake_A2B_cam_logit).to(self.device)) + self.BCE_loss(fake_B2B_cam_logit, torch.zeros_like(fake_B2B_cam_logit).to(self.device))
+            G_cam_loss_A = self.BCE_loss(fake_B2A_cam_logit, torch.ones_like(fake_B2A_cam_logit).to(self.device))
+            G_cam_loss_B = self.BCE_loss(fake_A2B_cam_logit, torch.ones_like(fake_A2B_cam_logit).to(self.device))
 
-            G_loss_A =  self.adv_weight * (G_ad_loss_GA + G_ad_cam_loss_GA + G_ad_loss_LA + G_ad_cam_loss_LA) + self.cycle_weight * G_recon_loss_A + self.identity_weight * G_identity_loss_A + self.cam_weight * G_cam_loss_A
-            G_loss_B = self.adv_weight * (G_ad_loss_GB + G_ad_cam_loss_GB + G_ad_loss_LB + G_ad_cam_loss_LB) + self.cycle_weight * G_recon_loss_B + self.identity_weight * G_identity_loss_B + self.cam_weight * G_cam_loss_B
+            if self.identity_weight > 0:
+                G_cam_loss_A += self.BCE_loss(fake_A2A_cam_logit, torch.zeros_like(fake_A2A_cam_logit).to(self.device))
+                G_cam_loss_B += self.BCE_loss(fake_B2B_cam_logit, torch.zeros_like(fake_B2B_cam_logit).to(self.device))
+
+            G_loss_A = self.adv_weight * (G_ad_loss_GA + G_ad_cam_loss_GA + G_ad_loss_LA + G_ad_cam_loss_LA) + self.cycle_weight * G_recon_loss_A + self.cam_weight * G_cam_loss_A
+            G_loss_B = self.adv_weight * (G_ad_loss_GB + G_ad_cam_loss_GB + G_ad_loss_LB + G_ad_cam_loss_LB) + self.cycle_weight * G_recon_loss_B + self.cam_weight * G_cam_loss_B
+
+            if self.identity_weight > 0:
+                G_loss_A += self.identity_weight * G_identity_loss_A
+                G_loss_B += self.identity_weight * G_identity_loss_B
 
             Generator_loss = G_loss_A + G_loss_B
             Generator_loss.backward()
