3ed33952732e44a44cdc96d0418dc31a1bc2e01d
wangruohui <12756472+wangruohui@users.noreply.github.com>
2021-09-22 19:56:44 +0800

    add run.sh script

--- run.sh
+++ run.sh
@@ -3,10 +3,14 @@
 DATE=`date "+%m%d-%H%M"`
 RESULT=result-${DATE}
 
+mkdir ${RESULT}
+echo "python main.py --light 8 --dataset selfie2anime -save_freq 8000 --result_dir $RESULT $@" > ${RESULT}.log
+
 # python main.py --light 8 --dataset selfie2anime --save_freq 8000 --print_freq 10
 python main.py \
     --light 8 \
     --dataset selfie2anime \
     --save_freq 8000 \
-    --result_dir $RESULT
-    2>&1 | tee ${RESULT}/log
+    --result_dir $RESULT \
+    "$@" #\
+#    2>&1 | tee ${RESULT}.log
